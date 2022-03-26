50f25c97e1e88b7a8c09639ec251be654bd6b2b4
pratheek87 <pratheek87@gmail.com>
2022-03-26 15:45:56 +0530

    changes

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://656422ca.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
